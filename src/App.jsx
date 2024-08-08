2eb266528ccf8feb5f4c01c3edb709bd477a4666
Arman <armanar6712@gmail.com>
2024-08-09 01:10:51 +0530

    Added Hero section and a responsive navbar components

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="relative h-full overflow-y-auto antialiased">
+      <div className="fixed inset-0 bg-fixed bg-cover bg-center bg-img"></div>
+      <div className="relative z-10 flex flex-col items-center p-4 space-y-8 container mx-auto">
+        <Hero />
+        <Navbar />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import logo from "../assets/logo.png";
+import { NAVIGATION_LINKS } from "../constants";
+import { FaTimes, FaBars } from "react-icons/fa";
+
+function Navbar() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const handleLinkClick = (e, href) => {
+    e.preventDefault();
+    const targetElement = document.querySelector(href);
+
+    if (targetElement) {
+      const offset = -85;
+      const elementPosition = targetElement.getBoundingClientReact().top;
+      const offsetPosition = elementPosition + window.scrollY + offset;
+
+      window.scrollTo({
+        top: offsetPosition,
+        behavior: "smooth",
+      });
+    }
+    setIsMobileMenuOpen(false);
+  };
+
+  return (
+    <div>
+      <nav className="fixed left-0 right-0 top-0 z-50">
+        {/* Desktop Nav */}
+        <div className="mx-auto hidden max-w-2xl items-center justify-center rounded-lg border border-stone-50/30 bg-black/20 py-3 backdrop-blur-lg lg:flex">
+          <div className="flex items-center justify-between gap-6">
+            <div>
+              <a href="#">
+                <img src={logo} alt="logo" width={150} />
+              </a>
+            </div>
+            <div>
+              <ul className="flex items-center gap-4">
+                {NAVIGATION_LINKS.map((item, index) => (
+                  <li key={index}>
+                    <a
+                      className="text-sm hover:text-yellow-400"
+                      href={item.href}
+                      onClick={(e) => {
+                        handleLinkClick(e, item.href);
+                      }}
+                    >
+                      {item.label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        </div>
+        {/* Mobile Nav */}
+        <div className="rounded-lg backdrop-blur-md lg:hidden">
+          <div className="flex items-center justify-between">
+            <div>
+              <a href="#">
+                <img src={logo} alt="Logo" width={90} className="m-2" />
+              </a>
+            </div>
+            <div className="flex items-center">
+              <button
+                className="focus:outline-none lg:hidden"
+                onClick={toggleMobileMenu}
+              >
+                {isMobileMenuOpen ? (
+                  <FaTimes className="m-2 h-6 w-5" />
+                ) : (
+                  <FaBars className="m-2 h-6 w-5" />
+                )}
+              </button>
+            </div>
+          </div>
+          {isMobileMenuOpen && (
+            <ul className="ml-4 mt-4 flex flex-col gap-4 backdrop-blur-md">
+              {NAVIGATION_LINKS.map((item, index) => (
+                <li key={index}>
+                  <a
+                    href={item.href}
+                    className="block w-full text-lg"
+                    onClick={(e) => {
+                      handleLinkClick(e, item.href);
+                    }}
+                  >
+                    {item.label}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;
